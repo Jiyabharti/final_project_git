6b5f48a7ea7e9ab91cc7fead089ce3678f0ac895
Bek <rebecca.e.rootes@gmail.com>
2024-04-08 13:24:26 +0100

    changes to structure, additional html pages, additional routes

--- application/routes.py
+++ application/routes.py
@@ -2,7 +2,53 @@ from flask import render_template
 
 from application import app
 
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('1_home.html', title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template('2_about.html', title="About Us")
+
+
+@app.route('/education')
+def education():
+    return render_template('3_education.html', title='Education Support')
+
+
+@app.route('/wellness')
+def wellness():
+    return render_template('4_wellness.html', title='Wellness Support')
+
+
+@app.route('/health')
+def health():
+    return render_template('5_health.html', title='Health Support')
+
+
+@app.route('/login')
+def login():
+    return render_template('6_login.html', title='User Log In')
+
+
+@app.route('/register')
+def register():
+    return render_template('7_register.html', title='User Registration')
+
+
+@app.route('/terms')
+def terms():
+    return render_template('terms-privacy-faq.html', id="terms")
+
+
+@app.route('/privacy')
+def privacy():
+    return render_template('terms-privacy-faq.html', id="privacy")
+
+
+@app.route('/faq')
+def faq():
+    return render_template('terms-privacy-faq.html', id="faq")
